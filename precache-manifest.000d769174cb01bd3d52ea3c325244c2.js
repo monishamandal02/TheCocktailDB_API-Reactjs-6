ab0951d14194817faf62aaa9899eaa9377c03734
monishamandal02 <monishamandal0203@gmail.com>
2021-06-01 16:11:22 +0530

    Updates

---
+++ precache-manifest.000d769174cb01bd3d52ea3c325244c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28350e99ddc19e67b62e8954974b203e",
+    "url": "/TheCocktailDB_API-Reactjs-6/index.html"
+  },
+  {
+    "revision": "6cce4a4d36158beb2ce1",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/css/main.f8a98f98.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cce4a4d36158beb2ce1",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/js/main.09b3c10b.chunk.js"
+  },
+  {
+    "revision": "482fe4e3bf2207a77f6a",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/js/runtime-main.e0473944.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/TheCocktailDB_API-Reactjs-6/static/media/logo.f3ad7664.svg"
+  }
+]);
